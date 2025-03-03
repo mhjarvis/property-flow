d748275bd833a16ea280975ad32b5eca4c92b99e
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-03-03 09:41:50 -0600

    Add link functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
 export default function HomePage() {
-	return <div>HomePage</div>
+	return (
+		<div>
+			<h1 className="text-5xl font-bold">Welcome</h1>
+			<Link href="/properties">Show Properties</Link>
+		</div>
+	)
 }
---
+++ app/properties/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function PropertiesPage() {
+	return (
+		<div>
+			<h1 className="text-5xl font-bold">Properties</h1>
+			<Link href="/">Go Home</Link>
+		</div>
+	)
+}
