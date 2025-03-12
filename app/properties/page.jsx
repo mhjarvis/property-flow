b50ff7173dd18fc2f5073a9a817b7d7c3e5a57ab
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-03-12 17:59:26 -0500

    Implement properties listing page with PropertyCard component and add properties data

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,20 @@
-import Link from "next/link"
+import PropertyCard from "@/components/PropertyCard"
+import properties from "@/properties.json"
 
 export default function PropertiesPage() {
 	return (
-		<div>
-			<h1 className="text-5xl font-bold">Properties</h1>
-			<Link href="/">Go Home</Link>
-		</div>
+		<section class="px-4 py-6">
+			<div class="container-xl lg:container m-auto px-4 py-6">
+				{properties.length === 0 ? (
+					<p> No properties found</p>
+				) : (
+					<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+						{properties.map((property, index) => (
+							<PropertyCard key={property._id} property={property} />
+						))}
+					</div>
+				)}
+			</div>
+		</section>
 	)
 }
