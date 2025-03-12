6c91e6192ac928be19b2d895e6ac778bd2fa08cf
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-03-12 17:19:56 -0500

    Show login/register button only when user is not logged in

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -229,9 +229,11 @@ export default function Navbar() {
 								Add Property
 							</Link>
 						)}
-						<button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-							<span>Login or Register</span>
-						</button>
+						{!isLoggedIn && (
+							<button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
+								<span>Login or Register</span>
+							</button>
+						)}
 					</div>
 				</div>
 			)}
