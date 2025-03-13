8f543a3176126a934ca96a552b3de3f6f97c24b1
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-03-12 22:10:17 -0500

    Log MongoDB URI in HomePage component for debugging purposes

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import Hero from "@/components/Hero"
 import HomeProperties from "@/components/HomeProperties"
 
 export default function HomePage() {
+	console.log(process.env.MONGODB_URI)
+
 	return (
 		<div>
 			<Hero />
