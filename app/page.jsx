091b13848c88aca998b4edd1e678d16162766997
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-03-12 21:39:45 -0500

    Add HomeProperties component to display recent properties on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import InfoBoxes from "@/components/InfoBoxes"
 import Hero from "@/components/Hero"
+import HomeProperties from "@/components/HomeProperties"
 
 export default function HomePage() {
 	return (
 		<div>
 			<Hero />
 			<InfoBoxes />
+			<HomeProperties />
 		</div>
 	)
 }
