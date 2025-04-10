727c09c1482e89f1480311d4f3bc4460832208c5
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-04-09 21:17:55 -0500

    Refactor authentication options to improve code clarity and structure

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,6 @@
+import connectDB from "@/config/database"
+import User from "@/models/User"
+
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
@@ -15,18 +18,34 @@ export const authOptions = {
 		}),
 	],
 	callbacks: {
-		// Invoked on successful sign-in
+		// Invoked on successful signin
 		async signIn({ profile }) {
-			// 1. connect to db
-			// 2 check if user exists
-			// 3. if not, create a new user in db
-			// 4. return true or false
+			// 1. Connect to database
+			await connectDB()
+			// 2. Check if user exists
+			const userExists = await User.findOne({ email: profile.email })
+			// 3. If not, then add user to database
+			if (!userExists) {
+				// Truncate user name if too long
+				const username = profile.name.slice(0, 20)
+
+				await User.create({
+					email: profile.email,
+					username,
+					image: profile.picture,
+				})
+			}
+			// 4. Return true to allow sign in
+			return true
 		},
-	},
-	// modifies the session object
-	async session({ session }) {
-		// 1. get user from db
-		// 2. assign userid to the session
-		// 3. return session
+		// Modifies the session object
+		async session({ session }) {
+			// 1. Get user from database
+			const user = await User.findOne({ email: session.user.email })
+			// 2. Assign the user id to the session
+			session.user.id = user._id.toString()
+			// 3. return session
+			return session
+		},
 	},
 }
